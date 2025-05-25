3227c93ffb354a6106798a50095e11986ee06613
fatimaKhd <12231897@students.liu.edu.lb>
2025-05-26 00:31:03 +0300

    Update app.py

--- backend/functions/app.py
+++ backend/functions/app.py
@@ -48,7 +48,10 @@ app = Flask(__name__)
 # CORS(app, supports_credentials=True)
 # CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True, methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
 # ✅ Allow only your Vercel frontend
-CORS(app, origins="*", supports_credentials=True)
+CORS(app, origins=[
+    r"https://bloodbank-[a-z0-9\-]+\.vercel\.app",
+    "http://localhost:8080"
+], supports_credentials=True)
 
 
 # Generate JWT Token
